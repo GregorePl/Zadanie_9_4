9a0614e436e9088f1f25c681e21c864a9f5aa0fa
gregveg <kuzniak.greg@gmail.com>
2017-09-06 15:28:41 +0200

    tree updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-  for(i = 1;i <= 10; i++){
-  var star = "";
-  for(j = i; j < 10; j++){
-    star += "*";
-  }
-    console.log(star);
-}  
-
+"use strict";
+function drawTree(pietra) { 
+    for (var i = 1; i <= pietra ; i++){ 
+    var star = ""; 
+    for (var j = 1; j <= i; j++){
+      star += "*";
+    }  
+      console.log(star);
+    }
+}
+drawTree(10)
